71e90754230f5280d849febe1cab2b0ae3647f63
Martin Podloucký <podlouckymartin@gmail.com>
2020-02-03 10:46:11 +0100

    Day 16 morning workout solution.

---
+++ day16-loops-objects/morning-workout-solution/index.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const aside = document.querySelector('#aside');
+  aside.addEventListener('mouseenter', () => {
+    aside.classList.remove('aside--closed');
+  });
+  aside.addEventListener('mouseleave', () => {
+    aside.classList.add('aside--closed');
+  });
+});
