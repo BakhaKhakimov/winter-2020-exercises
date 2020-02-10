995c06aa54f5e003dfd62f31b3623bcd4796394a
Martin Podloucký <podlouckymartin@gmail.com>
2020-02-10 14:02:59 +0100

    Solution to the morning workout of day 21.

---
+++ day21-intro-to-react/morning-workout-solution/index.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const app = document.querySelector('#app');
+
+  const contacts = [
+    new Contact('female', 'Patsy Gillan', '334-343-2987'),
+    new Contact('female', 'Lucille R. Smith', '517-813-8608'),
+  ];
+
+  contacts.forEach(contact => contact.mount(app));
+});
